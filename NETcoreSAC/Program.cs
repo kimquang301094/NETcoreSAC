d191cdf91c281bc678c2de3120cb496e3a715a52
kimquang301094 <41021454+kimquang301094@users.noreply.github.com>
2021-01-25 00:41:24 +0700

    Add files via upload
    FIRST time upload code. 
    Tính và Xuất tuổi dựa theo năm sinh.

---
+++ NETcoreSAC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace NETcoreSAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            int age = 1994;
+            Console.Write("Nhập năm sinh (đầy đủ 4 số): ");
+            
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Tuổi hiện tại là " + (Convert.ToInt32(DateTime.Now.ToString("yyyy"))-age) + " .");
+        }
+    }
+}j
